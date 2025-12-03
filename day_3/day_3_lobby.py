48e027ba9f943c3cfc5e7577748bee0115fb7834
John Fyhr <johnforslund90@gmail.com>
2025-12-03 19:01:09 +0100

    Day 3 part 2 finished.

--- day_3/day_3_lobby.py
+++ day_3/day_3_lobby.py
@@ -37,6 +37,7 @@ print(f"First 5 banks: {document[:5]}")
 
 # Goal is to find the maximum joltage of every bank, and sum them all up.
 
+""" TODO: LEGACY
 def calculate_total_output_joltage(document: list[str]) -> int:
     total_output_joltage = 0
 
@@ -46,8 +47,9 @@ def calculate_total_output_joltage(document: list[str]) -> int:
         total_output_joltage += bank_joltage
 
     return total_output_joltage
+"""
 
-
+""" TODO: LEGACY
 def calculate_max_bank_joltage(bank: str) -> int:
 
     # Find the first highest joltage battery
@@ -60,7 +62,7 @@ def calculate_max_bank_joltage(bank: str) -> int:
     assert max_bank_joltage == int(bank[first_battery_index] + bank[second_battery_index]), "Max bank joltage calculation error!"   # Sanity check
 
     return max_bank_joltage
-
+"""
 
 def get_highest_joltage_battery(bank: str, start_index: int, end_index: int) -> tuple[int, int]:
     """ Function to get highest joltage battery in a bank between start_index and end_index (inclusive) """
@@ -78,85 +80,45 @@ def get_highest_joltage_battery(bank: str, start_index: int, end_index: int) -> 
     return max_battery_index, max_joltage
 
 
-output = calculate_total_output_joltage(document)
-
-print(f"Output: {output}")
-
-
 
 ##############
 ##  Part 2  ##
 ##############
 
 
-# Same problem, but definitions of invalid ID's has changed:
-# An ID is invalid if it consists entirely of two OR MORE repeated integers (e.g. 11, 222, 123123123, 99999)
-# => if ID = XX[X...], where X is any integer.
+# Change: You can now turn on 12 batteries in every bank, from left to right.
+# So, instead of 2 batteries, it is now 12 batteries. Nothing else changed.
 
+# Let's make the functions more general to handle any number of batteries to turn on:
+def calculate_max_bank_joltage(bank: str, num_batteries: int) -> int:
 
+    # Initialize list to hold selected batteries' indices and joltages
+    battery_indices = []
+    battery_joltages = []
 
-# Initializing variables
-invalid_id_list = {}  # List of invalid ID's
-invalid_id_sum = 0  # Sum of invalid ID's
-
-for d in range(len(document)):
-    start_id = int(document[d].split('-')[0])
-    end_id = int(document[d].split('-')[1])
-
-    invalid_id_list[d] = []   # Initializing list for this range
-
-    # Convert to string for working with first and second half easier, as well as lengths
-    start_id_str = str(start_id)
-    end_id_str = str(end_id)
-
-    #if (len(start_id_str) == len(end_id_str)) and (len(start_id_str) % 2 != 0):
-    #    pass #continue  # All IDs in this range are odd in length, so skip to next range
-
-    for n in range(len(start_id_str), len(end_id_str) + 1):     # Looping through lengths of IDs
-        candidate_start = start_id
-        candidate_start_str = str(candidate_start)
-
-        candidate_end = end_id
-        candidate_end_str = str(candidate_end)
-
-        #if n % 2 != 0:                                          # If first length is odd,
-        #    continue                                            # Then skip it, continue with the loop
-
-        if n > len(start_id_str):                               # If not the start length, i.e. if we moved from start length to higher lengths
-            candidate_start = 10 ** (n - 1)                     # Then set new first ID to first integer of that length n (i.e. 100 for length 3, 1000 for length 4, etc.)
-            candidate_start_str = str(candidate_start)
-
-        if n < len(end_id_str):                                 # If not the end length, i.e. if we moved from end length to lower lengths
-            candidate_end_str = "9" * n                         # Then set new last ID to last integer of that length n (i.e. 999 for length 3, 9999 for length 4, etc.)
-            candidate_end = int(candidate_end_str)
+    # Loop through all the batteries to turn on
+    for b in range(num_batteries):
+        battery_index, battery_joltage = get_highest_joltage_battery(bank, 0 if b == 0 else battery_indices[-1] + 1, len(bank) - (num_batteries - b - 1))
+        battery_indices.append(battery_index)
+        battery_joltages.append(battery_joltage)
         
-        print(f"n: {n} out of {len(end_id_str)}")
+    max_bank_joltage = int(''.join(str(j) for j in battery_joltages))
+    assert max_bank_joltage == int(''.join(bank[i] for i in battery_indices)), "Max bank joltage calculation error!"   # Sanity check
+
+    return max_bank_joltage
 
 
-        # Loop through different split indices
-        for s in range(2, n + 1):                               # Looping through possible split sizes (s = number of repeats)
-            split_index = n / s
-            if not split_index.is_integer():
-                continue                                        # Skip non-integer split sizes
-            #assert split_index.is_integer(), "Split index is not integer!"    # Sanity check
-            split_index = int(split_index)
-            print(f"split_index: {split_index}")
+# And add the new parameter num_batteries when calling the main function:
+def calculate_total_output_joltage(document: list[str], num_batteries: int) -> int:
+    total_output_joltage = 0
 
-            candidate_half_id = candidate_start_str[:split_index]
-            candidate_id = int(candidate_half_id * s)               # Creating the first candidate ID by repeating the first half
+    # Loop through banks
+    for bank in document:
+        bank_joltage = calculate_max_bank_joltage(bank, num_batteries)
+        total_output_joltage += bank_joltage
 
-            while candidate_id <= candidate_end:                    # Ensure it doesn't exceed the (candidate) end ID
-                
-                if candidate_id in (range(start_id, end_id + 1)):     # Last check if candidate ID is within the original start and end ID range
-                    if candidate_id not in invalid_id_list[d]:
-                        invalid_id_list[d].append(candidate_id)
-                    print("Found invalid ID:", candidate_id)
-
-                candidate_half_id = str(int(candidate_half_id) + 1)   # Incrementing the first half's first integer by 1
-                candidate_id = int(candidate_half_id * s)
+    return total_output_joltage
 
 
-for k in invalid_id_list:
-    invalid_id_sum += sum(invalid_id_list[k])
-print(f"Output: {invalid_id_sum}")
-
+output = calculate_total_output_joltage(document, 12)  # Now specifying number of batteries to turn on
+print(f"Output: {output}")
